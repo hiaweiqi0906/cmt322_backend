40b3b6295a31b2961ea582672b40771721cbb131
JieYi-777 <limjieyi777@gmail.com>
2023-12-20 10:30:32 +0800

    Just send pull request first

--- routes/appointment.js
+++ routes/appointment.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController')
 
-<<<<<<< HEAD:routes/appointment.js
-router.get('/', appointmentController.adminGetAppointment)
-=======
 router.get('/admin/:username', appointmentController.adminGetAppointment)
 
 router.get('/userlist/:username', appointmentController.getUserList)
 
->>>>>>> b408519 (updating the existing code):routes/appointmentRoutes.js
 
 module.exports = router;
